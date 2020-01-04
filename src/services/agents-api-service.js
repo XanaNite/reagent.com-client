af1c6a42b95151b613afa9047ca86e92f6558a81
Amber Meritt <amber.merittcampbell@gmail.com>
2020-01-04 14:16:54 -0700

    added API/token services

---
+++ src/services/agents-api-service.js
@@ -0,0 +1,47 @@
+import TokenService from './token-service'
+import config from '../config'
+
+const AgentApiService = {
+    getAgents(){
+        return fetch(`${config.API_ENDPOINT}/agents`, {
+            headers: {
+              'authorization': `basic ${TokenService.getAuthToken()}`,
+            },
+        })
+        .then(res =>
+            (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        )
+    },
+    getAgent(agentId){
+        return fetch(`${config.API_ENDPOINT}/agents/${agentId}`, {
+            headers: {
+              'authorization': `basic ${TokenService.getAuthToken()}`,
+            },
+        })
+        .then(res =>
+            (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        )
+    },
+    postAgent(agentInfo){
+        return fetch(`${config.API_ENDPOINT}/agents`, {
+            method: 'POST',
+            headers: {
+              'authorization': `basic ${TokenService.getAuthToken()}`,
+            },
+            body: JSON.stringify({
+                agentInfo
+            }),
+        })
+        .then(res =>
+            (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        )
+    }
+}
+
+export default AgentApiService
