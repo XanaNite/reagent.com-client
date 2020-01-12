bc9122ea32df2aa48cd4190c65b8ac0ee0fb3399
Amber Meritt <amber.merittcampbell@gmail.com>
2020-01-12 14:55:45 -0700

    hide secrets add heroku endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000',
-    TOKEN_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c', //add API token
+    API_ENDPOINT: 'https://aqueous-lake-82914.herokuapp.com/',
+    TOKEN_KEY: process.env.REACT_APP_API_KEY,
   }
